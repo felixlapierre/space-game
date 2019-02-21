0255db4e58330c088f0e40cf97349b4b38ac4a58
Felix <felix.lapierre1@gmail.com>
2019-02-20 21:28:19 -0500

    Worked on movement, walking on walls, rotating character sprite

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -6,48 +6,100 @@ public class Player : MonoBehaviour
 {
     float speed = 10f;
     Rigidbody2D rb;
+    new Collider2D collider;
     int coins = 0;
+    float radius;
     Vector3 startingPosition; //teleport back to starting position on death
 
-    int jumps = 2;
+    int grounded = 0; //0 means false
+    int airjump = 1;
+    Vector2 facing;
+    Vector2 normalToSurface;
+
+    private bool Grounded
+    {
+        get { return grounded > 0; }
+    }
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>(); //Get my rigid body
+        var col = GetComponent<CircleCollider2D>();
+        collider = col;
+        radius = col.radius;
         startingPosition = transform.position;
+        facing = new Vector3(0, 1, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        var input = Input.GetAxis("Horizontal");
-        //var movement = input * speed;
-        //rb.velocity = new Vector3(movement, rb.velocity.y, 0);
-
-        if(Input.GetKeyDown(KeyCode.W))
+        Debug.Log("Grounded: " + (grounded > 0));
+        transform.eulerAngles = new Vector3(0, 0, VectorToAngle(normalToSurface) - 90.0f);
+        if(Grounded)
         {
-            rb.AddForce(new Vector3(0, 200, 0)); //Add force straight up
+            WalkOnWalls();
         }
-        if(Input.GetKeyDown(KeyCode.Space))
+
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            if (jumps > 0)
+            if (grounded > 0)
             {
-                jumps--;
-                var worldMousePosition = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y));
-                var direction = worldMousePosition - transform.position;
-                direction.z = 0;
-                direction.Normalize();
-                rb.velocity = direction * speed;
+                Jump();
+            }
+            else if(airjump > 0)
+            {
+                airjump--;
+                Jump();
             }
         }
     }
 
-    void OnTriggerEnter2D(Collider2D col) //col is the object we collided with
+    void WalkOnWalls()
     {
+        var inputX = Input.GetAxis("Horizontal");
+        var inputY = Input.GetAxis("Vertical");
+
+        if ((inputX != 0 || inputY != 0))
+        {
+            var input = new Vector2(inputX, inputY).normalized;
+            var movement = Vector3.Project(input, Vector2.Perpendicular(normalToSurface)).normalized;
+            movement *= speed;
+            rb.velocity = movement;
+        }
+        else
+        {
+            rb.velocity = Vector2.zero;
+        }
+    }
+
+    float VectorToAngle(Vector2 direction)
+    {
+        return 360 / 2 / Mathf.PI * Mathf.Atan2(direction.y, direction.x);
+    }
+
+    void Jump()
+    {
+        var worldMousePosition = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y));
+        var direction = worldMousePosition - transform.position;
+        direction.z = 0;
+        direction.Normalize();
+        facing = direction;
+        rb.velocity = direction * speed;
+        normalToSurface = rb.velocity.normalized;
+    }
+
+    void OnCollisionEnter2D(Collision2D collision) //col is the object we collided with
+    {
+        var col = collision.collider;
+        
         if(col.tag == "Wall")
         {
-            jumps = 2;
+            var contactPoint = collision.contacts[0];
+            normalToSurface = contactPoint.normal;
+            grounded++;
+            airjump = 1;
         }
         else if(col.tag == "Coin")
         {
@@ -71,11 +123,14 @@ public class Player : MonoBehaviour
         }
     }
 
-    void OnTriggerExit2D(Collider2D col)
+    void OnCollisionExit2D(Collision2D collision)
     {
+        var col = collision.collider;
         if(col.tag == "Wall")
         {
-
+            grounded--;
+            if (!Grounded)
+                normalToSurface = rb.velocity.normalized;
         }
     }
 }
